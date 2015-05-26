8cea8242b7ab64724ec75421a0c192e57e0fb5fe
Alex Dobson <alex.dobson@live.com>
2015-05-26 12:37:49 -0600

    Fixed an error with the version number in the readme.txt file and main PHP file.

--- trunk/dobsondev-shortcodes.php
+++ trunk/dobsondev-shortcodes.php
@@ -3,7 +3,7 @@
  * Plugin Name: DobsonDev Shortcodes
  * Plugin URI: http://dobsondev.com/portfolio/dobsondev-shortcodes/
  * Description: A collection of helpful shortcodes.
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: Alex Dobson
  * Author URI: http://dobsondev.com/
  * License: GPLv2
